feafcaeaf2ce5cd21a7feb84051caa65f56fcdf8
Fox <Fox@yuzutv.com>
2025-03-12 16:52:03 +0900

    不要なimportを削除

--- AllowanceList/app/src/main/java/com/example/allowancelist/MainActivity.kt
+++ AllowanceList/app/src/main/java/com/example/allowancelist/MainActivity.kt
@@ -16,14 +16,10 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.ime
-import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.text.KeyboardOptions
